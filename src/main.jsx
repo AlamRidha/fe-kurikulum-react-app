0c17b125a8266bdfd974e58b25b74c6dbb0d0052
AlamRidha <alamrridha@gmail.com>
2024-12-28 00:32:35 +0700

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { createBrowserRouter } from "react-router";
+
+// router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Router Berhasil</div>,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
